ee51b1b0f451bf65290600e55822a1495d5e3c64
kadziedzioch <93613725+kadziedzioch@users.noreply.github.com>
2023-10-21 19:10:24 +0200

    initalise project

---
+++ calcit/src/main/java/com/example/calcit/CalcitApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcitApplication.class, args);
+	}
+
+}
---
+++ calcit/src/test/java/com/example/calcit/CalcitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calcit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
