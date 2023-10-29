69d50799fdf00890a49308b1cb3a2b2a94381069
hrdsl21 <hrdslws@gmail.com>
2023-10-29 19:49:22 +0100

    AddLoginForm

---
+++ calcit/src/main/java/controller/AddLoginForm.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AddLoginForm {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
