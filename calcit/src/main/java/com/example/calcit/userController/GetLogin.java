900367c3d8a383e47bdafefec70d42bc5d13f45a
hrdsl21 <hrdslws@gmail.com>
2023-10-30 08:02:57 +0100

    AddLoginForm

--- calcit/src/main/java/com/example/calcit/userController/GetLogin.java
+++ calcit/src/main/java/com/example/calcit/userController/GetLogin.java
@@ -1,13 +1,14 @@
-package controller;
+package com.example.calcit.userController;
+
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class AddLoginForm {
-
+public class GetLogin {
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
+
     }
 }
