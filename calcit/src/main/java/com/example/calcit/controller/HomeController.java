825c340435035295c38d745d480a1397460cc244
kadziedzioch <93613725+kadziedzioch@users.noreply.github.com>
2023-11-15 19:51:05 +0100

    add navigation to register and login

--- calcit/src/main/java/com/example/calcit/controller/HomeController.java
+++ calcit/src/main/java/com/example/calcit/controller/HomeController.java
@@ -2,17 +2,11 @@ package com.example.calcit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
     
-     @GetMapping("/")
+    @GetMapping("/")
     public String showHomePage() {
         return "index";
     }
