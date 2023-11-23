6fb0de7a0b74e7649cecc09b10bf4a069d1e5621
kadziedzioch <93613725+kadziedzioch@users.noreply.github.com>
2023-11-23 19:59:52 +0100

    refactor db

--- calcit/src/main/java/com/example/calcit/model/BMI.java
+++ calcit/src/main/java/com/example/calcit/model/BMI.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
  public class BMI implements Serializable{
 
     @Id
- 	@GeneratedValue(strategy = GenerationType.AUTO)
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
  	@Column(name="bmi_id")
  	private int bmiId;
 
