e34e7abeae0e20b7985e8120b555712cbed2dc50
hrdsl21 <hrdslws@gmail.com>
2023-11-29 18:47:36 +0100

    addMysql

--- calcit/src/main/java/com/example/calcit/controller/PRALController.java
+++ calcit/src/main/java/com/example/calcit/controller/PRALController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.calcit.model.BMI;
-import com.example.calcit.model.BMIResult;
+
 import com.example.calcit.model.PRAL;
 import com.example.calcit.model.PRALResult;
 import com.example.calcit.service.PRALService;
